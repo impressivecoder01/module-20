361062889720cd8d675ccf401f923c3d093fb36c
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-15 03:16:14 +0600

    first commit

---
+++ class2.js
@@ -0,0 +1,8 @@
+// unit convert inch to feet, miles to kilometer
+// 12 inch 1 feet
+function inchToFeet(inch){
+    let feet = inch/12;
+    return feet;
+}
+const verify = 75;
+console.log(inchToFeet(verify))
