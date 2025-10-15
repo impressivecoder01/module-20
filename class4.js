57fb5b63e1c3b9338cef14aa5057a272c9fe14e1
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-16 02:55:40 +0600

    class4DOne

--- class4.js
+++ class4.js
@@ -9,15 +9,18 @@ function oddAvg(numbers){
     return sum;
 }
 let sum2 = 0;
+let newAry = []
 function evenAvg(numbers){
     for(let number of numbers){
         if(number % 2 ===0){
             sum2 = sum2 + number
+            newAry.push(number)
         }
     }
-    return sum2;
+    let result = sum2/newAry.length;
+    return result;
 }
 const a = [2,1,43,5,6,432,1,];
-const b = [2,1,1,2,1];
+const b = [4,1,1,6,1];
 console.log(oddAvg(a))
 console.log(evenAvg(b))
