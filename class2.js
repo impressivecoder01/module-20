0d2b55162bc29376e2d19340b8f002321f6f36e5
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-16 02:19:50 +0600

    class2Done

--- class2.js
+++ class2.js
@@ -6,3 +6,20 @@ function inchToFeet(inch){
 }
 const verify = 75;
 console.log(inchToFeet(verify))
+
+function inchToFeet2(inch){
+    const feetFraction = inch/12;
+    const feetNumber = parseInt(feetFraction)
+    const inchRemain = inch % 12;
+    const reslt = feetNumber + 'ft' + inchRemain + 'inch'
+    return reslt;
+}
+const shuvo = inchToFeet2(40);
+console.log(shuvo)
+
+function milesToKilometers(mile){
+    const kilo = mile * 1.60934;
+    return kilo;
+
+}
+console.log(milesToKilometers(55))
