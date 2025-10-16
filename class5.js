7a7457bb43f2f3b29830137489a72cbf86551515
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-17 00:19:23 +0600

    class6Added

--- class5.js
+++ class5.js
@@ -3,7 +3,7 @@ const biriyani = ['a', 'b', 'c', 'c', 'd', 'a'];
 const numbers = [1,2,2,43,43,1,76];
 function noDuplicate (array){
     const unique = [];
-    for(let biri of biriyani){
+    for(let biri of array){
         if(unique.includes(biri)=== false){
             unique.push(biri)
         }
@@ -11,3 +11,4 @@ function noDuplicate (array){
     return unique;
 }
 console.log(noDuplicate(biriyani))
+console.log(noDuplicate(numbers))
---
+++ class6.js
@@ -0,0 +1,19 @@
+//math, abs, round, ceil, floor, and random number
+const mn = Math.min(34,5343,1,345,6);
+console.log(mn)
+const mx = Math.max(45,323,54,32,1,46);
+console.log(mx)
+const pi = 3* Math.PI;
+console.log(pi)
+const m = -80;
+console.log(Math.abs(m))
+
+const round = 5.4543;
+const roundd = 5.55;
+console.log(Math.round(round))
+console.log(Math.round(roundd))
+console.log(Math.floor(round))
+console.log(Math.ceil(roundd))
+console.log(Math.random()*100)
+console.log(Math.random())
+console.log(Math.round(Math.random()*6))
