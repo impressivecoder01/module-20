0b8a5ce4f7a89c0731d6ae781de39f5aebb7b4c9
impressivecoder1 <rohanrahman251@gmail.com>
2025-10-17 22:09:54 +0600

    pblmdate

---
+++ class7.js
@@ -0,0 +1,6 @@
+//a simple introduction to js data and time
+const today = new Date();
+console.log(today)
+const date = new Date('2050-10-19');
+console.log(date.getDate(),'d')
+console.log(date.getMonth())
